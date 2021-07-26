5271230965930cc76f75d468235eee22866cb094
Marko <mare.radovanovic90@gmail.com>
2021-07-26 22:47:28 +0200

    made some chnages to the main app.controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,11 +3,8 @@ import { Controller, Get } from '@nestjs/common';
 @Controller()
 export class AppController {
   @Get()        // http://localhost:3000/
-  getHello(): string {
-    return 'Hello World!';
+  getIndex(): string {
+    return 'Home page!';
   }
-  @Get('world') // http://localhost:3000/world/
-  getWorld(): string {
-    return 'World!';
-  }
+
 }
