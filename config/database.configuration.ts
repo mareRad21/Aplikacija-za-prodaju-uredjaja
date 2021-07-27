30e07e88cf3acb0ac9ca52cc153a30b0038051fc
Marko <mare.radovanovic90@gmail.com>
2021-07-27 18:07:07 +0200

    Code upload

---
+++ config/database.configuration.ts
@@ -0,0 +1,6 @@
+export const databaseConfiguration = {
+    hostname: 'localhost',
+    username: 'aplikacija',
+    password: 'aplikacija',
+    database: 'aplikacija'
+};
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { databaseConfiguration } from 'config/database.configuration';
+import { Administrator } from 'entities/administrator.entity';
 import { AppController } from './app.controller';
+import { AdministratorService } from './services/administrator/administrator.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: databaseConfiguration.hostname,
+      username: databaseConfiguration.username,
+      password: databaseConfiguration.password,
+      database: databaseConfiguration.database,
+      port: 3306,
+      entities: [Administrator]
+    })
+  ],
   controllers: [AppController],
-  providers: [],
+  providers: [AdministratorService],
 })
 export class AppModule {}
