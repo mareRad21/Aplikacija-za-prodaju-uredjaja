2a736863b8456b2cc892f364351ce848dce6ca87
Marko <mare.radovanovic90@gmail.com>
2021-11-22 23:20:43 +0100

    Implement the user registration mechanism

--- src/services/user/user.service.ts
+++ src/services/user/user.service.ts
@@ -1,18 +1,40 @@
 import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
+import { TypeOrmCrudService } from "@nestjsx/crud-typeorm";
+import { UserRegistrationDto } from "src/dtos/user/user.registration.dto";
 import { User } from "src/entities/user.entity";
+import { ApiResponse } from "src/misc/api.response.class";
 import { Repository } from "typeorm";
+import * as crypto from "crypto";
 
 @Injectable()
-export class UserService{
-    constructor(
-        @InjectRepository(User) 
-        private readonly user: Repository<User>
-    ){}
-    getAll(): Promise<User[]>{
-        return this.user.find();
+export class UserService extends TypeOrmCrudService<User>{
+    constructor(@InjectRepository(User) private readonly user: Repository<User>){
+        super(user);
     }
-    getById(id: number): Promise<User>{
-        return this.user.findOne(id);
+
+    async register(data: UserRegistrationDto): Promise<User|ApiResponse>{
+        const passwordHash = crypto.createHash('sha512');
+        passwordHash.update(data.password);
+        const passwordHashSting = passwordHash.digest('hex').toUpperCase();
+
+        const newUser: User   = new User();
+        newUser.email         = data.email;
+        newUser.passwordHash  = passwordHashSting;
+        newUser.forename      = data.forename;
+        newUser.surname       = data.surname;
+        newUser.phoneNumber   = data.phoneNumber;
+        newUser.postalAddress = data.postalAddress;
+        
+        try {
+            const savedUser = await this.user.save(newUser);
+            if(!savedUser){
+                throw new Error("");
+            }
+
+            return savedUser;
+        } catch (error) {
+            return new ApiResponse('error', -6001, "This user account cannot be created!")
+        }
     }
 }
